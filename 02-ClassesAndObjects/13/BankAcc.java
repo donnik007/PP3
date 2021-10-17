e54b857e08475ce7e675a221f6756466d6971ed9
donnik007 <s218161@student.uek.krakow.pl>
2021-10-17 21:01:29 +0200

    zadanko 13

---
+++ 02-ClassesAndObjects/13/BankAcc.java
@@ -0,0 +1,21 @@
+public class BankAcc
+{
+   String bank;
+   float money;
+   int accNumber;
+   boolean isCredial;
+   String accName;
+   
+   void moneyStatus(){
+        System.out.print(money);
+    };
+    
+   void showNum(){
+        System.out.print(accNumber);
+    };
+
+   void isCredit(){
+        System.out.print(isCredial);
+    };
+    
+}
---
+++ 02-ClassesAndObjects/13/Smartphone.java
@@ -0,0 +1,20 @@
+public class Smartphone
+{
+    int freeMem;
+    String model;
+    String producent;
+    boolean isWorking;
+    float mpx;
+    
+    void isWorking(){
+        System.out.print("WORKING");
+    };
+    
+    void showModel(){
+        System.out.print(model);
+    };
+    
+    void showProd(){
+        System.out.print(producent);
+    };
+}
